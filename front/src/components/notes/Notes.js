0efc6ffa257e455e7f2bb8a6e4dbbc99269cebcf
Ubuntu <calota.denisa14@gmail.com>
2020-01-15 14:48:23 +0000

    buttons, textbox added. Markdown editor with bugs added

--- front/src/components/notes/Notes.js
+++ front/src/components/notes/Notes.js
@@ -1,26 +1,68 @@
 import React from "react";
 import "./Notes.css";
-import logo from "../../assets/images/logo.png";
-import { Button,  Label, Input} from "reactstrap";
-// import Datetime from 'react-datetime';
+import { Button,  Input, Label, FormGroup, Card,CardBody, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import MdEditor from 'react-markdown-editor-lite'
+import MarkdownIt from 'markdown-it'
 
 class Notes extends React.Component {
+    mdParser = null;
+    constructor(props) {
+        super(props)
+        this.mdParser = new MarkdownIt(/* Markdown-it options */)
+    }
+  
+    handleEditorChange ({html, text}) {    
+        console.log('handleEditorChange', html, text)
+    }
+
   render() {
-    return <div className = " NotesComponent">
-        <div className="buttons">
+      
+    return (<div className = "NotesComponent">
+        <Row>
             <Button color="info" size="sm">Distribuie</Button>
             <Button color="success" size="sm">Salveaza</Button>
             <Button color="danger" size="sm" className="button"> Sterge </Button>
-        </div> 
+        </Row> 
         
-            <div className="noteDetails">
-                <Input id="title" placeholder="Titlu"/>
-                <Input id="title" placeholder="Materie"/>
-            </div>
-            
-       
-    </div>;
+        
+        <Row>
+            <Col >
+                <Input id="titlu" placeholder="Titlu"/>
+            </Col>
+        </Row>
+        
+        <Row>
+            <Col >
+                <Input id="materie" placeholder="Materie"/>
+            </Col>
+        </Row>    
+        
+        
+          <FormGroup check inline className="form-check-radio">
+            <Label className="form-check-label">
+                <Input type="radio" name="exampleRadios1" id="exampleRadios11" value="option1" />
+                Curs
+                <span className="form-check-sign"></span>
+            </Label>
+          </FormGroup>
+          <FormGroup check inline className="form-check-radio">
+            <Label className="form-check-label">
+              <Input type="radio" name="exampleRadios1" id="exampleRadios12" value="option2" defaultChecked/>
+              Seminar
+              <span className="form-check-sign"></span>
+            </Label>
+          </FormGroup>
+          
+           
+      <div >
+            <MdEditor
+                renderHTML={(text) => this.mdParser.render(text)}
+                onChange={this.handleEditorChange} 
+            />              
+      </div> 
+        
+    </div>);
   }
 }
 
