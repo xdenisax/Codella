efeaa0fd237fd4183e454bdabcba1cd28c04e5d8
Ubuntu <calota.denisa14@gmail.com>
2020-01-15 19:46:34 +0000

    Resolved bug on markdown. Getting input from form

--- front/src/components/notes/Notes.js
+++ front/src/components/notes/Notes.js
@@ -7,60 +7,114 @@ import MarkdownIt from 'markdown-it'
 
 class Notes extends React.Component {
     mdParser = null;
+    
     constructor(props) {
         super(props)
-        this.mdParser = new MarkdownIt(/* Markdown-it options */)
+        this.state={
+            id:-1,
+            keywordInput:"",
+            keywords: [],
+            titleInput:"",
+            subjectInput:"",
+            tagInput:"Seminar",
+            markdownInput:""
+        }; 
+        this.mdParser = new MarkdownIt();
+        this.note = props.note;
     }
   
-    handleEditorChange ({html, text}) {    
-        console.log('handleEditorChange', html, text)
+    handleEditorChange = (html, text) => {    
+        this.setState({markdownInput: text.target.defaultValue});
     }
+    
+    
+    onChangeTitleInput = (event)=>{
+        this.setState({ titleInput: event.target.value});
+    }
+    
+    onChangeSubjectInput = (event) =>{
+        this.setState( {subjectInput:  event.target.value});
+    }
+    
+    onChangeTagInput = (event)=>{
+        this.setState({tagInput: event.target.value});
+        console.log(this.state.tagInput);
+    }
+    
+    onChangeKeywordInput = (event)=>{
+        this.setState({keywordInput: event.target.value});
+    }
+    
+    tokenizeKeywords=()=>{
+        this.setState({keywords:this.state.keywordInput.split(",")});
+        console.log(this.state.keywords);
+        return this.state.keywords;
+        //console.log(this.state.keywordInput.split(","));
+    }
+    
+    saveNote=()=>{
+        if(this.state.id>0){
+            //actualizam notita in bd
+            console.log("actualizare");
+            console.log(this.state.titleInput, this.state.subjectInput, this.state.tagInput, this.state.markdownInput);
+            
+        }else{
+            //salvam o notita noua
+            this.tokenizeKeywords();
+            console.log("notita noua");
+            console.log(this.state.titleInput, this.state.subjectInput, this.state.tagInput, this.state.keywords, this.state.markdownInput);
+            
+        }
+    }
+    
 
   render() {
       
     return (<div className = "NotesComponent">
         <Row>
             <Button color="info" size="sm">Distribuie</Button>
-            <Button color="success" size="sm">Salveaza</Button>
+            <Button color="success" size="sm" onClick={this.saveNote} >Salveaza</Button>
             <Button color="danger" size="sm" className="button"> Sterge </Button>
         </Row> 
         
         
         <Row>
             <Col >
-                <Input id="titlu" placeholder="Titlu"/>
+                <Input id="titlu" placeholder="Titlu" onChange={this.onChangeTitleInput}/>
             </Col>
         </Row>
         
         <Row>
             <Col >
-                <Input id="materie" placeholder="Materie"/>
+                <Input id="materie" placeholder="Materie" onChange = {this.onChangeSubjectInput}/>
             </Col>
         </Row>    
         
         
           <FormGroup check inline className="form-check-radio">
             <Label className="form-check-label">
-                <Input type="radio" name="exampleRadios1" id="exampleRadios11" value="option1" />
-                Curs
+                <Input type="radio" name="exampleRadios1" id="exampleRadios11" value="Curs" onChange={this.onChangeTagInput}/>
+                    Curs
                 <span className="form-check-sign"></span>
             </Label>
           </FormGroup>
           <FormGroup check inline className="form-check-radio">
             <Label className="form-check-label">
-              <Input type="radio" name="exampleRadios1" id="exampleRadios12" value="option2" defaultChecked/>
-              Seminar
+              <Input type="radio" name="exampleRadios1" id="exampleRadios12" value="Seminar" defaultChecked onChange={this.onChangeTagInput}/>
+                    Seminar
               <span className="form-check-sign"></span>
             </Label>
           </FormGroup>
           
+          <Input placeholder="Cuvinte cheie" onChange={this.onChangeKeywordInput}/>
+          
            
-      <div >
-            <MdEditor
-                renderHTML={(text) => this.mdParser.render(text)}
-                onChange={this.handleEditorChange} 
-            />              
-      </div> 
+          <div >
+                <MdEditor
+                    renderHTML={(text) => this.mdParser.render(text)}
+                    onChange={this.handleEditorChange} 
+                />              
+          </div> 
         
     </div>);
   }
